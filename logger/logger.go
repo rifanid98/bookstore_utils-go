66e9889bb5f3c0c4ab7524626089bce4965c6dfc
rifanid98 <adninsijawa.office@gmail.com>
2021-07-11 17:45:33 +0700

    refactor logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,11 +1,24 @@
 package logger
 
 import (
+	"fmt"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var Log *zap.Logger
+var log = &logger{}
+
+type iLogger interface {
+	// elastic logger
+	Printf(string, ...interface{})
+	// mysql logger
+	Print(...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
 
 func init() {
 	config := zap.Config{
@@ -23,18 +36,34 @@ func init() {
 	}
 
 	var err error
-	if Log, err = config.Build(); err != nil {
+	if log.log, err = config.Build(); err != nil {
 		panic(err)
 	}
 }
 
+func GetLogger() *logger {
+	return log
+}
+
+func (l *logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func (l *logger) Print(v ...interface{}) {
+	Info(fmt.Sprintf("%v", v))
+}
+
 func Info(msg string, tags ...zap.Field) {
-	Log.Info(msg, tags...)
-	Log.Sync()
+	log.log.Info(msg, tags...)
+	log.log.Sync()
 }
 
 func Error(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("error", err))
-	Log.Error(msg, tags...)
-	Log.Sync()
+	log.log.Error(msg, tags...)
+	log.log.Sync()
 }
